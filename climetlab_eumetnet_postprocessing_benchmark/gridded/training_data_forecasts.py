2e12ad3cb27dca9d89be51bfa41c93ebc604e7b5
Jonathan Demaeyer <jodemaey@meteo.be>
2022-01-24 07:48:37 +0100

    Update of the version number

--- climetlab_eumetnet_postprocessing_benchmark/gridded/training_data_forecasts.py
+++ climetlab_eumetnet_postprocessing_benchmark/gridded/training_data_forecasts.py
@@ -16,7 +16,7 @@ from climetlab.indexing import PerUrlIndex
 from ..config import baseurl
 from ..utils import convert_to_datetime
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 _terms_of_use = """By downloading data from this dataset, you agree to the terms and conditions defined at
 
