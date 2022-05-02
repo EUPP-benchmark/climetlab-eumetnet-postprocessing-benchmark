6a6839d75c4ca8ea384241ba27906e86394d4b7d
Jonathan Demaeyer <jodemaey@meteo.be>
2022-05-02 16:04:45 +0200

    Preparing release to v0.1.7

--- climetlab_eumetnet_postprocessing_benchmark/gridded/training_data_reforecasts.py
+++ climetlab_eumetnet_postprocessing_benchmark/gridded/training_data_reforecasts.py
@@ -13,7 +13,7 @@ from .training_data_forecasts import TrainingDataForecast, TrainingDataForecastS
     TrainingDataForecastSurfacePostProcessed
 from ..utils import convert_to_datetime
 
-__version__ = "0.1.6"
+__version__ = "0.1.7"
 
 # TODO: Add a check for valid reforecast date and then warn the user if not valid
 
