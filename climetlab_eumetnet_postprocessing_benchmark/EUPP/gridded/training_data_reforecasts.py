3a9700293b71782dc9276817ea8f1875ab449023
Jonathan Demaeyer <jodemaey@meteo.be>
2025-09-12 15:50:14 +0200

    Releasing v0.3

--- climetlab_eumetnet_postprocessing_benchmark/EUPP/gridded/training_data_reforecasts.py
+++ climetlab_eumetnet_postprocessing_benchmark/EUPP/gridded/training_data_reforecasts.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 from .training_data_forecasts import TrainingDataForecastSurface, TrainingDataForecastPressure,\
     TrainingDataForecastSurfaceProcessed
 
-__version__ = "0.2.8"
+__version__ = "0.3.0"
 
 
 class TrainingDataReforecastSurface(TrainingDataForecastSurface):
